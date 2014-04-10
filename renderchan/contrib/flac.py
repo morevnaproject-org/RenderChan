1bdc0c20c9452e048c0a4cc9245775069734796c
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2014-04-10 22:30:08 +0700

    Flac audio module

---
+++ renderchan/contrib/flac.py
@@ -0,0 +1,54 @@
+
+
+__author__ = 'Konstantin Dmitriev'
+
+from renderchan.module import RenderChanModule
+from renderchan.utils import which
+import subprocess
+import os
+import re
+import random
+
+class RenderChanFlacModule(RenderChanModule):
+    def __init__(self):
+        RenderChanModule.__init__(self)
+        self.conf['binary']="flac"
+        self.conf['sox_binary']="sox"
+        self.conf["packetSize"]=0
+
+    def getInputFormats(self):
+        return ["mp3"]
+
+    def getOutputFormats(self):
+        return ["wav"]
+
+    def getDependencies(self, filename):
+        return []
+
+    def checkRequirements(self):
+        for key in ['binary','sox_binary']:
+            if which(self.conf[key]) == None:
+                self.active=False
+            else:
+                self.active=True
+        return self.active
+
+    def render(self, filename, outputPath, startFrame, endFrame, width, height, format, audioRate, compatVersion, updateCompletion):
+
+        comp = 0.0
+        updateCompletion(comp)
+
+        random_string = "%08d" % (random.randint(0,99999999))
+        tmpfile=outputPath+"."+random_string
+
+        # TODO: Progress callback
+
+        commandline=["flac", "-d", filename, "-o", tmpfile]
+        subprocess.check_call(commandline)
+
+        commandline=["sox", tmpfile, outputPath, "rate", "-v", audioRate]
+        subprocess.check_call(commandline)
+
+        os.remove(tmpfile)
+
+        updateCompletion(1.0)
