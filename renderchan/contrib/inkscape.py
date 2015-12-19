97bd4ffdbb7833f1028ed266b662c776dcb0579d
scribblemaniac <scribblemaniac@users.noreply.github.com>
2015-12-19 11:57:16 -0700

    Added dependency analysis for inkscape

--- renderchan/contrib/inkscape.py
+++ renderchan/contrib/inkscape.py
@@ -5,6 +5,7 @@ __author__ = 'scribblemaniac'
 from renderchan.module import RenderChanModule
 from renderchan.utils import which
 import subprocess
+import gzip
 import os
 import re
 import random
@@ -25,19 +26,30 @@ class RenderChanInkscapeModule(RenderChanModule):
         return ["png", "ps", "eps", "pdf", "emf", "wmf"]
 
     def analyze(self, filename):
-        #TODO: Consider embeded images dependencies
-        return {}
+        info={"dependencies":[]}
 
+        if filename.endswith(".svgz"):
+            f=gzip.open(filename)
+        else:
+            f=open(filename)
+
+        linkPattern = re.compile(".*sodipodi:absref=\"(.*?)\".*")
+        for line in f.readlines():
+            pat=linkPattern.search(line)
+            if pat:
+                info["dependencies"].append(pat.group(1).strip())
+        f.close
+
+        return info
 
     def checkRequirements(self):
-        for key in ['binary']:
-            if which(self.conf[key]) == None:
-                self.active=False
-                print "Module warning (%s): Cannot find '%s' executable." % (self.getName(), self.conf[key])
-                print "    Please install pencil2d package."
-                return False
-        self.active=True
-        return True
+        if which(self.conf['binary']) == None:
+            self.active=False
+            print "Module warning (%s): Cannot find '%s' executable." % (self.getName(), self.conf["binary"])
+            print "    Please install inkscape package."
+        else:
+            self.active=True
+        return self.active
 
     def render(self, filename, outputPath, startFrame, endFrame, format, updateCompletion, extraParams={}):
 
