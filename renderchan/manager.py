b3bcf57d7096c59314e2860d2f519d1b2fc38eeb
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2016-01-17 20:47:28 +0600

    Temporary disable --freeze and --unfreeze functionality

--- renderchan/manager.py
+++ renderchan/manager.py
@@ -15,12 +15,13 @@ def process_args():
         usage=_("""
     %prog               """))
 
-    parser.add_option("--freeze", dest="freezeList",
-            action="append",
-            help=_("Freeze path."))
-    parser.add_option("--unfreeze", dest="unfreezeList",
-            action="append",
-            help=_("Un-freeze path."))
+    # The --freeze and --unfreeze options are temporary disabled, because this function should behave differently.
+    #parser.add_option("--freeze", dest="freezeList",
+    #        action="append",
+    #        help=_("Freeze path."))
+    #parser.add_option("--unfreeze", dest="unfreezeList",
+    #        action="append",
+    #        help=_("Un-freeze path."))
     parser.add_option("--lang", dest="setLanguage",
             action="store", nargs=1,
             help=_("Switch project language."))
@@ -37,27 +38,28 @@ def main(argv):
     options, args = process_args()
 
     # Parse frozen parameters
-    if options.freezeList or options.unfreezeList:
-        renderchan = RenderChan()
-        if not options.freezeList:
-            options.freezeList=[]
-        if not options.unfreezeList:
-            options.unfreezeList=[]
-        frozenListChanged=False
-        for filename in options.freezeList:
-            filename=os.path.abspath(filename)
-            if not filename in options.unfreezeList:
-                taskfile = RenderChanFile(filename, renderchan.modules, renderchan.projects)
-                taskfile.setFrozen(True)
-                frozenListChanged=True
-        for filename in options.unfreezeList:
-            filename=os.path.abspath(filename)
-            if not filename in options.freezeList:
-                taskfile = RenderChanFile(filename, renderchan.modules, renderchan.projects)
-                taskfile.setFrozen(False)
-                frozenListChanged=True
-        if frozenListChanged:
-            taskfile.project.saveFrozenPaths()
+    # The --freeze and --unfreeze options are temporary disabled, because this function should behave differently.
+    #if options.freezeList or options.unfreezeList:
+    #    renderchan = RenderChan()
+    #    if not options.freezeList:
+    #        options.freezeList=[]
+    #    if not options.unfreezeList:
+    #        options.unfreezeList=[]
+    #    frozenListChanged=False
+    #    for filename in options.freezeList:
+    #        filename=os.path.abspath(filename)
+    #        if not filename in options.unfreezeList:
+    #            taskfile = RenderChanFile(filename, renderchan.modules, renderchan.projects)
+    #            taskfile.setFrozen(True)
+    #            frozenListChanged=True
+    #    for filename in options.unfreezeList:
+    #        filename=os.path.abspath(filename)
+    #        if not filename in options.freezeList:
+    #            taskfile = RenderChanFile(filename, renderchan.modules, renderchan.projects)
+    #            taskfile.setFrozen(False)
+    #            frozenListChanged=True
+    #    if frozenListChanged:
+    #        taskfile.project.saveFrozenPaths()
 
     if options.setLanguage:
         project = RenderChanProject(os.getcwd())
