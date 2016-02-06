ea337e777c3dd5abcbbf3ae03bad34671554d38a
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2016-02-06 17:25:52 +0600

    Fix desktop integration for Dolphin 15.x (KDE)

--- desktop/kde-actions/install.sh
+++ desktop/kde-actions/install.sh
@@ -8,10 +8,23 @@ PREFIX="$1"
 fi
 
 if [[ `id -u` == "0" ]]; then
-DEST="$PREFIX/share/kde4/services/ServiceMenus"
+DEST1="$PREFIX/share/kde4/services/ServiceMenus/"
+DEST2="$PREFIX/share/kservices5/ServiceMenus/"
+DEST3="$PREFIX/share/kservicetypes5/"
 else
-DEST="$HOME/.kde/share/kde4/services/ServiceMenus/"
+DEST1="$HOME/.kde/share/kde4/services/ServiceMenus/"
+DEST2="$HOME/.local/share/kservices5/ServiceMenus/"
+DEST3="$HOME/.local/share/kservicetypes5/"
 fi
 
-mkdir -p "$DEST" || true
-install -D -m 644 "$SCRIPTPATH/renderchan.desktop" "$DEST"
+mkdir -p "$DEST1" || true
+mkdir -p "$DEST2" || true
+install -D -m 644 "$SCRIPTPATH/renderchan.desktop" "$DEST1"
+install -D -m 644 "$SCRIPTPATH/renderchan.desktop" "$DEST2"
+
+if [ ! -f "/usr/share/kservicetypes5/konqpopupmenuplugin.desktop" ] && \
+   [ ! -f "/usr/local/share/kservicetypes5/konqpopupmenuplugin.desktop" ] && \
+   [ ! -f "$HOME/.local/share/kservicetypes5/konqpopupmenuplugin.desktop" ]
+then
+    install -D -m 644 "$SCRIPTPATH/konqpopupmenuplugin.desktop" "$DEST3"
+fi
