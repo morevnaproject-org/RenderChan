d19d1b6f8f0651a5fcec5d247b47484d130b2bb3
scribblemaniac <scribblemaniac@users.noreply.github.com>
2016-05-21 22:58:49 -0600

    Removed unzip dependency and optimized krita module

--- renderchan/contrib/krita.py
+++ renderchan/contrib/krita.py
@@ -4,7 +4,8 @@ from renderchan.module import RenderChanModule
 import subprocess
 import random
 import os
-import shutil
+import tempfile
+from zipfile import ZipFile
 from xml.etree import ElementTree
 from renderchan.utils import which
 from renderchan.utils import mkdirs
@@ -13,12 +14,10 @@ class RenderChanKritaModule(RenderChanModule):
     def __init__(self):
         RenderChanModule.__init__(self)
         if os.name == 'nt':
-            self.conf['zip_binary']=os.path.join(os.path.dirname(__file__),"..\\..\\..\\zip\\bin\\unzip.exe")
             self.conf['convert_binary']=os.path.join(os.path.dirname(__file__),"..\\..\\..\\imagemagick\\bin\\convert.exe")
             self.conf['binary']=os.path.join(os.path.dirname(__file__),"..\\..\\..\\krita\\bin\\krita.exe")
         else:
             #TODO: Additional bunaries should be separate modules
-            self.conf['zip_binary']="unzip"
             self.conf['convert_binary']="convert"
             self.conf['binary']="krita"
 
@@ -39,11 +38,6 @@ class RenderChanKritaModule(RenderChanModule):
             print("Module warning (%s): Cannot find '%s' executable." % (self.getName(), self.conf['binary']))
             print("    Please install krita package.")
             return False
-        if which(self.conf['zip_binary']) == None:
-            self.active=False
-            print("Module warning (%s): Cannot find '%s' executable!" % (self.getName(), self.conf['zip_binary']))
-            print("    Please install unzip package.")
-            return False
         if which(self.conf['convert_binary']) == None:
             self.active=False
             print("Module warning (%s): Cannot find '%s' executable!" % (self.getName(), self.conf['convert_binary']))
@@ -54,30 +48,21 @@ class RenderChanKritaModule(RenderChanModule):
 
     def analyze(self, filename):
         info={'dependencies':[], 'width':0, 'height':0}
-
-        random_string = "%08d" % (random.randint(0,99999999))
-        #TODO: fix temporary directory path for Windows case!
-        tmpPath=os.path.join("/tmp","renderchan-krita-module-info-"+random_string)
-        mkdirs(tmpPath)
-
-        commandline=[self.conf['zip_binary'], "-j", "-d", tmpPath, filename, "maindoc.xml"]
-        subprocess.check_call(commandline)
-
-        f=open(os.path.join(tmpPath,"maindoc.xml"), "rb")
-
-        #TODO: Consider file layers as dependencies
-        #...
         
-        tree = ElementTree.parse(f)
-        root = tree.getroot()
+        with tempfile.TemporaryDirectory() as tmpPath:
+            with ZipFile(filename) as zip:
+                mdPath = zip.extract('maindoc.xml', tmpPath)
+            
+            with open(mdPath, "rb") as f:
+                #TODO: Consider file layers as dependencies
+                #...
+                
+                tree = ElementTree.parse(f)
+                root = tree.getroot()
 
-        # Extracting width/height
-        info["width"] = root.find("{http://www.calligra.org/DTD/krita}IMAGE").attrib["width"]
-        info["height"] = root.find("{http://www.calligra.org/DTD/krita}IMAGE").attrib["height"]
-        
-        f.close()
-
-        shutil.rmtree(tmpPath)
+                # Extracting width/height
+                info["width"] = root.find("{http://www.calligra.org/DTD/krita}IMAGE").attrib["width"]
+                info["height"] = root.find("{http://www.calligra.org/DTD/krita}IMAGE").attrib["height"]
 
         return info
 
@@ -92,32 +77,20 @@ class RenderChanKritaModule(RenderChanModule):
                 deps_count+=1
                 break
 
-        random_string = "%08d" % (random.randint(0,99999999))
-        outputPathTmp = outputPath + "-" + random_string + "." + format
+        with tempfile.TemporaryDirectory() as tmpPath:
+            outputPathTmp = os.path.join(tmpPath, "image." + format)
+            if deps_count==0:
+                with ZipFile(filename) as zip:
+                    zip.extract('mergedimage.png', tmpPath)
+                #TODO: Compress image?
+                os.rename(os.path.join(tmpPath,"mergedimage.png"), outputPathTmp)
+            else:
+                #TODO: PNG transperency settings at ~/.kde/share/config/kritarc ? use KDEHOME env ?
+                commandline=[self.conf['binary'], "--export", filename, "--export-filename", outputPathTmp]
+                subprocess.check_call(commandline)
 
-        if deps_count==0:
-
-            random_string = "%08d" % (random.randint(0,99999999))
-            tmpPath=outputPath+"."+random_string
-            mkdirs(tmpPath)
-
-            commandline=[self.conf['zip_binary'], "-j", "-d", tmpPath, filename, "mergedimage.png"]
+            dimensions = extraParams["width"]+"x"+extraParams["height"]
+            commandline=[self.conf['convert_binary'], outputPathTmp, "-resize", dimensions, outputPath]
             subprocess.check_call(commandline)
-
-            #TODO: Compress image?
-            os.rename(os.path.join(tmpPath,"mergedimage.png"), outputPathTmp)
-
-            shutil.rmtree(tmpPath)
-
-        else:
-            #TODO: PNG transperency settings at ~/.kde/share/config/kritarc ? use KDEHOME env ?
-            commandline=[self.conf['binary'], "--export", filename, "--export-filename", outputPathTmp]
-            subprocess.check_call(commandline)
-
-        dimensions = extraParams["width"]+"x"+extraParams["height"]
-        commandline=[self.conf['convert_binary'], outputPathTmp, "-resize", dimensions, outputPath]
-        subprocess.check_call(commandline)
-
-        os.remove(outputPathTmp)
 
         updateCompletion(1)
