morevnaproject-org/RenderChan

commit c3e4141664a76425d6d681c795c6c09c37cd966e

Implement two options to control dimensions of output
1. 'use_own_dimensions' - If set to '1'. then RenderChan uses dimensions of source file.
This is supported only if module can retrieve width and height of file.
See analyze() in krita.py file for sample.
This prameter can defined for particular module (format). For example it is set to '1' for Krita module.
It is can be overriden for individual files through .conf file.
Default value: '0'

When 'use_own_dimensions' is set to '1' we need some way to control the scaling of the output depending on the profile. So here goes a second option:

2. 'proxy_scale' - This parameter is applied only if 'use_own_dimensions' set to '1'.
It applies the scaling factor to the output.
In usual case this option should be set in project.conf file for individual profiles.
For example, 'draft' profile can have 'proxy_scale' = '0.5'
Default value: '1.0'

Important: If dimensions, produced by scaling, are not integer values, then no scaling is applied. This is done to avoid distortions for image proportions.

renderchan/file.py
@@ -30,8 +30,10 @@ class RenderChanFile():
 
         self.module = modules.getByExtension(os.path.splitext(self.localPath)[1][1:])
         self.dependencies=[]
+        #TODO: startFrame and endFrame should go into self.config
         self.startFrame=-1
         self.endFrame=-1
+
         self.metadata=None
 
         self.config={}
@@ -57,6 +59,10 @@ class RenderChanFile():
                     self.startFrame=int(info["startFrame"])
                     self.endFrame=int(info["endFrame"])
                     self.dependencies=self.project.cache.getDependencies(self.localPath)
+                    if info["width"]>0:
+                        self.config['width']=str(info["width"])
+                    if info["height"]>0:
+                        self.config['height']=str(info["height"])
                 else:
                     info=self.module.analyze(self.getPath())
                     if "dependencies" in info.keys():
@@ -67,9 +73,19 @@ class RenderChanFile():
                     if "endFrame" in info.keys():
                         self.endFrame=int(info["endFrame"])
 
+                    if "width" in info.keys():
+                        self.config['width']=str(info["width"])
+                    else:
+                        info["width"] = -1
+
+                    if "height" in info.keys():
+                        self.config['height']=str(info["height"])
+                    else:
+                        info["height"] = -1
+
                     # Write cache
                     if self.project:
-                        self.project.cache.write(self.localPath, self.getTime(), self.startFrame, self.endFrame, self.dependencies)
+                        self.project.cache.write(self.localPath, self.getTime(), self.startFrame, self.endFrame, self.dependencies, info["width"], info["height"])
 
                 # Rendering params
                 if os.path.exists(self.getPath()+".conf"):
@@ -267,6 +283,25 @@ class RenderChanFile():
                     params[key]=float(self.config[key][1:])*float(params[key])
                 else:
                     params[key]=self.config[key]
+
+
+        # Special routines related with proxies
+        if 'use_own_dimensions' in params.keys() and 'proxy_scale' in params.keys():
+            if params['use_own_dimensions']!='0' and 'width' in params.keys() and 'height' in params.keys():
+                if params['proxy_scale']!='1.0':
+                    try:
+                        proxy_scale = float(params['proxy_scale'])
+                    except:
+                        print "WARNING: Wrong value for 'proxy scale' (%s)." % self.getPath()
+                        proxy_scale = 1.0
+                    width=int(params['width'])
+                    height=int(params['height'])
+                    if ((width*proxy_scale) % 1) != 0 or ((height*proxy_scale) % 1) != 0:
+                        print "WARNING: Can't apply 'proxy scale' for file (%s):" % self.getPath()
+                        print "         Dimensions %sx%s give non-integer values when multiplied by factor of %s." % (width, height, proxy_scale)
+                    else:
+                        params['width'] = str(width*proxy_scale)
+                        params['height'] = str(height*proxy_scale)
 
         # File-specific configuration
         #params["filename"]=self.getPath()

renderchan/cache.py
@@ -36,7 +36,7 @@ class RenderChanCache():
             self.connection.text_factory = str
             cur=self.connection.cursor()
 
-            cur.execute("CREATE TABLE IF NOT EXISTS Paths(Id INTEGER PRIMARY KEY, Path TEXT, Timestamp REAL, Start INTEGER, End INTEGER);")
+            cur.execute("CREATE TABLE IF NOT EXISTS Paths(Id INTEGER PRIMARY KEY, Path TEXT, Timestamp REAL, Start INTEGER, End INTEGER, Width INTEGER, Height INTEGER);")
             cur.execute("CREATE TABLE IF NOT EXISTS Dependencies(Id INTEGER, Dependency TEXT);")
             self.connection.commit()
 
@@ -77,6 +77,8 @@ class RenderChanCache():
                 info['timestamp']=row[2]
                 info['startFrame']=row[3]
                 info['endFrame']=row[4]
+                info['width']=row[5]
+                info['height']=row[6]
                 return info
             else:
                 return None
@@ -106,7 +108,7 @@ class RenderChanCache():
             print "ERROR: Cannot read from database."
             return None
 
-    def write(self, path, timestamp, start, end, dependencies):
+    def write(self, path, timestamp, start, end, dependencies, width, height):
         if self.closed:
             print "ERROR: Database is closed. Writing isn't possible."
             return None
@@ -122,7 +124,7 @@ class RenderChanCache():
             self.connection.commit()
 
             # Now, write the data
-            cur.execute("INSERT INTO Paths(Path, Timestamp, Start, End) VALUES(?, ?, ?, ?)", (path, timestamp, start, end))
+            cur.execute("INSERT INTO Paths(Path, Timestamp, Start, End, Width, Height) VALUES(?, ?, ?, ?, ?, ?)", (path, timestamp, start, end, width, height))
             id = cur.lastrowid
 
             # Again, make sure we have no associated data in dependency table

renderchan/contrib/krita.py
@@ -5,6 +5,7 @@ import subprocess
 import random
 import os
 import shutil
+import re
 from renderchan.utils import mkdirs
 
 class RenderChanKritaModule(RenderChanModule):
@@ -12,12 +13,18 @@ class RenderChanKritaModule(RenderChanModule):
         RenderChanModule.__init__(self)
         if os.name == 'nt':
             self.conf['zip_binary']=os.path.join(os.path.dirname(__file__),"..\\..\\..\\zip\\bin\\unzip.exe")
+            self.conf['convert_binary']=os.path.join(os.path.dirname(__file__),"..\\..\\..\\imagemagick\\bin\\convert.exe")
             self.conf['binary']=os.path.join(os.path.dirname(__file__),"..\\..\\..\\krita\\bin\\krita.exe")
         else:
+            #TODO: Additional bunaries should be separate modules
             self.conf['zip_binary']="unzip"
+            self.conf['convert_binary']="convert"
             self.conf['binary']="krita"
 
         self.conf["packetSize"]=0
+
+        self.extraParams['use_own_dimensions']='1'
+        self.extraParams['proxy_scale']='1.0'
 
     def getInputFormats(self):
         return ["kra"]
@@ -26,8 +33,41 @@ class RenderChanKritaModule(RenderChanModule):
         return ["png"]
 
     def analyze(self, filename):
+        info={'dependencies':[], 'width':0, 'height':0}
+
+        random_string = "%08d" % (random.randint(0,99999999))
+        #TODO: fix temporary directory path for Windows case!
+        tmpPath=os.path.join("/tmp","renderchan-krita-module-info-"+random_string)
+        mkdirs(tmpPath)
+
+        commandline=[self.conf['zip_binary'], "-j", "-d", tmpPath, filename, "maindoc.xml"]
+        subprocess.check_call(commandline)
+
+        f=open(os.path.join(tmpPath,"maindoc.xml"))
+
         #TODO: Consider file layers as dependencies
-        return {}
+        #...
+
+        # Extracting width/height
+        imagePattern = re.compile(".*<IMAGE .*>.*")
+
+        for line in f.readlines():
+            pat=imagePattern.search(line)
+            if pat:
+                widthPattern = re.compile(".*width=\"(.*?)\".*")
+                pat=widthPattern.search(line)
+                if pat:
+                    info["width"]=int(pat.group(1).strip())
+                heightPattern = re.compile(".*height=\"(.*?)\".*")
+                pat=heightPattern.search(line)
+                if pat:
+                    info["height"]=int(pat.group(1).strip())
+                break
+        f.close
+
+        shutil.rmtree(tmpPath)
+
+        return info
 
     def render(self, filename, outputPath, startFrame, endFrame, format, updateCompletion, extraParams={}):
 
@@ -40,6 +80,9 @@ class RenderChanKritaModule(RenderChanModule):
                 deps_count+=1
                 break
 
+        random_string = "%08d" % (random.randint(0,99999999))
+        outputPathTmp = outputPath + "-" + random_string + "." + format
+
         if deps_count==0:
 
             random_string = "%08d" % (random.randint(0,99999999))
@@ -50,13 +93,19 @@ class RenderChanKritaModule(RenderChanModule):
             subprocess.check_call(commandline)
 
             #TODO: Compress image?
-            os.rename(os.path.join(tmpPath,"mergedimage.png"), outputPath)
+            os.rename(os.path.join(tmpPath,"mergedimage.png"), outputPathTmp)
 
             shutil.rmtree(tmpPath)
 
         else:
             #TODO: PNG transperency settings at ~/.kde/share/config/kritarc ? use KDEHOME env ?
-            commandline=[self.conf['binary'], "--export", filename, "--export-filename", outputPath]
+            commandline=[self.conf['binary'], "--export", filename, "--export-filename", outputPathTmp]
             subprocess.check_call(commandline)
+
+        dimensions = extraParams["width"]+"x"+extraParams["height"]
+        commandline=[self.conf['convert_binary'], outputPathTmp, "-resize", dimensions, outputPath]
+        subprocess.check_call(commandline)
+
+        os.remove(outputPathTmp)
 
         updateCompletion(1)

renderchan/module.py
@@ -62,13 +62,21 @@ class RenderChanModule():
     def __init__(self):
         self.conf = {}
         self.conf['binary']=""
+        #TODO: "packetSize" should be renamed to "extra_params" and merged with self.extraParams?
         self.conf["packetSize"]=20
         self.conf["compatVersion"]=1
         self.conf["maxNbCores"]=0
 
         # Extra params - additional rendering parameters. supplied through the project.conf and
         # file-specific .conf files.
+        #TODO: ExtraParams should be registered in a special way
+        #TODO:    So, if a module registers its own extra param we should now it isn't already registered as global extraParam.
         self.extraParams={}
+        #   'use_own_dimensions' - Don't use project dimensions, use image dimensions defined it the source file
+        #   'proxy_scale' - Apply scale factor to resulting image. Valid only if 'use_own_dimensions' == 1.
+        #  Those options are applied in RenderChanFile.getParams() - file.py.
+        self.extraParams['use_own_dimensions']='0'
+        self.extraParams['proxy_scale']='1.0'
 
         self.active=False
 
@@ -124,6 +132,8 @@ class RenderChanModule():
         return []
 
     def analyze(self, filename):
+        #TODO: Return a special structure object (RenderChanFileInfo) instead of ordinary dictionary
+        #TODO:     RenderChanFileInfo = {'dependencies': [], 'start': -1, 'end': -1, 'width':-1, 'height':-1}
         return {}
 
     def getPacketSize(self):

renderchan/project.py
@@ -86,7 +86,7 @@ class RenderChanProject():
         # PROJECT CACHE
 
         # Check for cache version
-        self.cache_version = 1
+        self.cache_version = 2
         cachepath = os.path.join(self.path, "render", "cache.sqlite")
         if not readonly:
             if os.path.exists(os.path.join(self.path, "render", "cache.version")):
@@ -99,7 +99,7 @@ class RenderChanProject():
                         existing_cache_version=int(content[0].strip())
                     except:
                         pass
-                if existing_cache_version!=self.cache_version:
+                if existing_cache_version!=self.cache_version and os.path.exists(cachepath):
                     os.remove(cachepath)
             else:
                 if os.path.exists(cachepath):

commit 00d5ef050b65fd4555d3c13b0719c4301cae200d

Fix bug when profile dimensions are overriden by file dimensions (introduced in c3e4141664 and revealed by bd833cf03)

renderchan/contrib/synfig.py
@@ -146,7 +146,7 @@ class RenderChanSynfigModule(RenderChanModule):
 
         commandline.append(filename)
 
-        #print " ".join(commandline)
+        #print(" ".join(commandline))
         out = subprocess.Popen(commandline, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=0)
         rc = None
         #currentFrame = None

renderchan/file.py
@@ -33,6 +33,8 @@ class RenderChanFile():
         #TODO: startFrame and endFrame should go into self.config
         self.startFrame=-1
         self.endFrame=-1
+        self.width=0
+        self.height=0
 
         self.metadata=None
 
@@ -60,9 +62,9 @@ class RenderChanFile():
                     self.endFrame=int(info["endFrame"])
                     self.dependencies=self.project.cache.getDependencies(self.localPath)
                     if info["width"]>0:
-                        self.config['width']=str(info["width"])
+                        self.width=str(info["width"])
                     if info["height"]>0:
-                        self.config['height']=str(info["height"])
+                        self.height=str(info["height"])
                 else:
                     info=self.module.analyze(self.getPath())
                     if "dependencies" in info.keys():
@@ -74,18 +76,18 @@ class RenderChanFile():
                         self.endFrame=int(info["endFrame"])
 
                     if "width" in info.keys():
-                        self.config['width']=str(info["width"])
+                        self.width=str(info["width"])
                     else:
-                        info["width"] = -1
+                        self.width=0
 
                     if "height" in info.keys():
-                        self.config['height']=str(info["height"])
+                        self.height=str(info["height"])
                     else:
-                        info["height"] = -1
+                        self.height=0
 
                     # Write cache
                     if self.project:
-                        self.project.cache.write(self.localPath, self.getTime(), self.startFrame, self.endFrame, self.dependencies, info["width"], info["height"])
+                        self.project.cache.write(self.localPath, self.getTime(), self.startFrame, self.endFrame, self.dependencies, self.width, self.height)
 
                 # Rendering params
                 if os.path.exists(self.getPath()+".conf"):
@@ -284,6 +286,9 @@ class RenderChanFile():
                 else:
                     params[key]=self.config[key]
 
+        if 'use_own_dimensions' in params.keys() and is_true_string(params['use_own_dimensions']):
+            params['width']=self.width
+            params['height']=self.height
 
         # Special routines related with proxies
         if 'use_own_dimensions' in params.keys() and 'proxy_scale' in params.keys():