2573ca6b2fca0b733740212cfd80714d6d0a0f02
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2023-05-25 16:15:22 +0700

    feat(opentoonz): Add possibility to render to avi format (#78)

--- renderchan/contrib/opentoonz.py
+++ renderchan/contrib/opentoonz.py
@@ -7,6 +7,7 @@ from renderchan.utils import which
 import subprocess
 import os
 import random
+import shutil
 
 class RenderChanOpentoonzModule(RenderChanModule):
     def __init__(self):
@@ -25,19 +26,34 @@ class RenderChanOpentoonzModule(RenderChanModule):
         return ["tnz"]
 
     def getOutputFormats(self):
-        return ["png", "tiff"]
+        return ["png", "tiff", "avi"]
 
     def render(self, filename, outputPath, startFrame, endFrame, format, updateCompletion, extraParams={}):
 
         updateCompletion(0.0)
-
-        if not os.path.exists(outputPath):
-            os.mkdir(outputPath)
+        
+        if format == "avi":
+            img_format="tiff"
+            img_outputPath=outputPath+"."+img_format
+        else:
+            img_format=format
+            img_outputPath=outputPath
+        
+        if os.path.exists(img_outputPath):
+            shutil.rmtree(img_outputPath)
+        os.mkdir(img_outputPath)
 
         # TODO: Progress callback
 
         os.chdir(os.path.dirname(self.conf['binary']))
-        commandline=[self.conf['binary'], filename, "-o", os.path.join(outputPath, "image."+format), "-nthreads", extraParams['nthreads'], "-step", extraParams['step'], "-shrink", extraParams['shrink'], "-multimedia", extraParams['multimedia']]
+        
+        commandline=[self.conf['binary'], filename, "-o", os.path.join(img_outputPath, "image."+img_format), "-nthreads", extraParams['nthreads'], "-step", extraParams['step'], "-shrink", extraParams['shrink'], "-multimedia", extraParams['multimedia']]
         subprocess.check_call(commandline)
+        
+        if format == "avi":
+            #TODO: Detect frame rate!
+            commandline=[self.findBinary("ffmpeg"), "-r", "24", "-f", "image2", "-i", os.path.join(img_outputPath, "image.%04d."+img_format), outputPath]
+            subprocess.check_call(commandline)
+            shutil.rmtree(img_outputPath)
 
         updateCompletion(1.0)
