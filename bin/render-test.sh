8b02f5f7815eeaba1542d32c8f95901259685125
sam2much96 <sam2much96@gmail.com>
2024-03-13 02:36:21 +0100

    modified:   bin/render-test.sh

--- bin/render-test.sh
+++ bin/render-test.sh
@@ -1,9 +1,13 @@
+# Shot 1 frames
 ./renderchan '../projects/Dystopia/inputs/shot 1/shot 1 frame by frame .kra' && cp -r "../projects/Dystopia/render/inputs/shot 1/shot 1 frame by frame .kra.png/" "../projects/Dystopia/scenes/"
 
-
+# SHot 1 Props
 ./renderchan '../projects/Dystopia/extras/Truck 1/Truck.kra' && cp -r "../projects/Dystopia/render/extras/Truck 1" "../projects/Dystopia/scenes/" 
 
+# Backgrounds
+./renderchan "../projects/Dystopia/extras/backgrounds/raw 3 Dystopia Bckgrnd-1.psd"
 
+# Shot 1 Scene
 ./renderchan "../projects/Dystopia/scenes/Shot 1.tnz"
 
 ./renderchan "../projects/Dystopia/project.blend"
